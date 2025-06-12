fd88a03fe668df275afb4bd23292f182d56fe948
Jacob Dowdy <jacob@mac.lan>
2025-06-12 09:27:48 -0400

    minor changes to variables demo

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -1,11 +1,9 @@
 /* eslint-disable */
 
-// A variable is a named memory location that can hold a value. Variables can
-// be used to store a wide range of data types, such as numbers, strings, and
-// arrays. A variable is declared by specifying its name, data type, and
-// optionally an initial value. Once a variable is declared, it can be read
-// potentially updated in other parts of the program.
-//
-// Useful links:
-// https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
-//
+const courseName = "typescript";
+const courseName2 = 'typescript';
+
+const courseName3 = `typescript`;
+
+const amount = 10;
+const fraction = 10.5;
